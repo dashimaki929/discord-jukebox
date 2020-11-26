bedb55bbcb0eb02c5b72a79f02bef3c3a3dd31c0
dashimaki929 <akira.odashima.sys@gmail.com>
2020-11-27 01:17:14 +0900

    fixed index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const fs = require("fs");
 const config = JSON.parse(fs.readFileSync("./config/settings.json", "utf8"));
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const ytdl = require("ytdl-core-discord");
-const joiningVoiceChannels = {};
 
 client.on("ready", () => {
   console.log("Boot completed successfully!");
@@ -16,9 +14,6 @@ client.on("message", async (message) => {
    * Format: `!summon`
    */
   if (message.content === "!summon") {
-    if (message.member.voice.channel) {
-      setVCConnection(message.member.voice.channel, message.guild.id);
-    }
   }
 
   /**
@@ -27,11 +22,6 @@ client.on("message", async (message) => {
    * Format: `!leave`
    */
   if (message.content === "!leave") {
-    const connection = await getVCConnection(
-      message.member.voice.channel,
-      message.guild.id
-    );
-    connection.channel.leave();
   }
 
   /**
@@ -40,35 +30,7 @@ client.on("message", async (message) => {
    * Format: `!play {URL}`
    */
   if (message.content.startsWith("!play")) {
-    const [command, url] = message.content.split(" ");
-
-    const connection = await getVCConnection(
-      message.member.voice.channel,
-      message.guild.id
-    );
-
-    // TODO: ytdl-core-discord ではなく ytdl-core ライブラリへ変更, localから読み込み後再生へ変更
-    connection.play(await ytdl(url), { type: "opus" });
   }
 });
 
 client.login(config.settings.bot.token);
-
-/**
- * 接続中のボイスチャットコネクションを取得
- * 
- * @param {*} channel 
- * @param {*} guildId 
- */
-async function getVCConnection(channel, guildId) {
-  if (!joiningVoiceChannels[guildId]) {
-    await setVCConnection(channel, guildId);
-  }
-
-  return joiningVoiceChannels[guildId];
-}
-function setVCConnection(channel, guildId) {
-  channel.join().then((connection) => {
-    joiningVoiceChannels[guildId] = connection;
-  });
-}
